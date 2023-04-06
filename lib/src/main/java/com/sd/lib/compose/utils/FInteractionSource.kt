a5e7c2d4d142217460ab0dc92be0c54324ee82f9
zj565061763 <565061763@qq.com>
2023-04-06 15:13:43 +0800

    fCollectIsTouchedAsState

---
+++ lib/src/main/java/com/sd/lib/compose/utils/FInteractionSource.kt
@@ -0,0 +1,28 @@
+package com.sd.lib.compose.utils
+
+import androidx.compose.foundation.interaction.DragInteraction
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.runtime.*
+
+@Composable
+fun InteractionSource.fCollectIsTouchedAsState(): State<Boolean> {
+    val isTouched = remember { mutableStateOf(false) }
+    LaunchedEffect(this) {
+        val listInteraction = mutableListOf<Interaction>()
+        interactions.collect { interaction ->
+            when (interaction) {
+                is PressInteraction.Press -> listInteraction.add(interaction)
+                is PressInteraction.Release -> listInteraction.remove(interaction.press)
+                is PressInteraction.Cancel -> listInteraction.remove(interaction.press)
+
+                is DragInteraction.Start -> listInteraction.add(interaction)
+                is DragInteraction.Stop -> listInteraction.remove(interaction.start)
+                is DragInteraction.Cancel -> listInteraction.remove(interaction.start)
+            }
+            isTouched.value = listInteraction.isNotEmpty()
+        }
+    }
+    return isTouched
+}
