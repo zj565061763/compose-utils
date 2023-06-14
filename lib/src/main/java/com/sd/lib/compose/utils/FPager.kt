5eb24b9a655f29c17622f8174cebb8c7f213e08d
zj565061763 <565061763@qq.com>
2023-06-14 14:28:06 +0800

    FSyncIndex

--- lib/src/main/java/com/sd/lib/compose/utils/FPager.kt
+++ lib/src/main/java/com/sd/lib/compose/utils/FPager.kt
@@ -4,23 +4,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.snapshotFlow
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun PagerState.FSyncIndex(
-    index: MutableState<Int>,
-    anim: Boolean = false,
-) {
-    FSyncIndex(
-        index = index.value,
-        anim = anim,
-        onChange = {
-            index.value = it
-        },
-    )
-}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
