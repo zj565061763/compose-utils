904a76c68768926f76a72cd4da23135cad06aaae
zjun <565061763@qq.com>
2023-12-26 15:41:19 +0800

    fResString

--- lib/src/main/java/com/sd/lib/compose/utils/ResString.kt
+++ lib/src/main/java/com/sd/lib/compose/utils/ResString.kt
@@ -1,33 +1,52 @@
 package com.sd.lib.compose.utils
 
-import android.content.Context
+import android.content.res.Resources
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun fResString(vararg items: Any): String {
-    val context = LocalContext.current
-    var result by remember { mutableStateOf(items.toResString(context)) }
-    LaunchedEffect(items) {
-        result = items.toResString(context)
-    }
-    return result
-}
-
-private fun Array<*>.toResString(context: Context): String {
-    return buildString {
-        for (item in this@toResString) {
-            val itemString = when (item) {
-                is Int -> context.getString(item)
-                is String -> item
-                else -> item.toString()
+    val resources = resources()
+    return when (items.size) {
+        0 -> ""
+        1 -> items.first().anyToResString(resources)
+        else -> {
+            var result by remember { mutableStateOf(items.itemsToResString(resources)) }
+            LaunchedEffect(items) {
+                result = items.itemsToResString(resources)
             }
-            append(itemString)
+            result
         }
     }
+}
+
+private fun Array<*>.itemsToResString(resources: Resources): String {
+    return buildString {
+        for (item in this@itemsToResString) {
+            append(item.anyToResString(resources))
+        }
+    }
+}
+
+private fun Any?.anyToResString(resources: Resources): String {
+    return when (this) {
+        null -> ""
+        is String -> this
+        is Int -> resources.getString(this)
+        else -> this.toString()
+    }
+}
+
+@Composable
+@ReadOnlyComposable
+private fun resources(): Resources {
+    LocalConfiguration.current
+    return LocalContext.current.resources
 }
