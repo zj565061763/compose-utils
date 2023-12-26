6a10a16ad9d57568b2cc1bcf78afdebcc067dbcb
zjun <565061763@qq.com>
2023-12-27 03:28:17 +0800

    test

--- app/src/test/java/com/sd/demo/compose_utils/ExampleUnitTest.kt
+++ app/src/test/java/com/sd/demo/compose_utils/ExampleUnitTest.kt
@@ -2,8 +2,6 @@ package com.sd.demo.compose_utils
 
 import org.junit.Test
 
-import org.junit.Assert.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -12,6 +10,42 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val content = "1xxx1xxx1x"
+        kotlin.run {
+            val list = content.splitWithDelimiter("1")
+            println(list)
+        }
     }
+}
+
+private fun CharSequence.splitWithDelimiter(
+    delimiter: String,
+    ignoreCase: Boolean = false,
+): List<String> {
+    var currentOffset = 0
+    var nextIndex = indexOf(delimiter, currentOffset, ignoreCase)
+    if (nextIndex == -1) {
+        return listOf(this.toString())
+    }
+
+    if (nextIndex == 0 && length == delimiter.length) {
+        return listOf(this.toString())
+    }
+
+    val result = ArrayList<String>(10)
+    do {
+        val substring = substring(currentOffset, nextIndex)
+        if (substring.isNotEmpty()) {
+            result.add(substring)
+        }
+        result.add(delimiter)
+        currentOffset = nextIndex + delimiter.length
+        nextIndex = indexOf(delimiter, currentOffset, ignoreCase)
+    } while (nextIndex != -1)
+
+    val substring = substring(currentOffset, length)
+    if (substring.isNotEmpty()) {
+        result.add(substring)
+    }
+    return result
 }
