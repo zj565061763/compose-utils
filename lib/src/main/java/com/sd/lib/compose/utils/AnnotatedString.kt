340373269d7f56b4212f532a9f19441f2fb2e98f
zjun <565061763@qq.com>
2023-12-26 21:34:06 +0800

    fAnnotatedWithTarget

---
+++ lib/src/main/java/com/sd/lib/compose/utils/AnnotatedString.kt
@@ -0,0 +1,54 @@
+package com.sd.lib.compose.utils
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.buildAnnotatedString
+
+fun String.fAnnotatedWithTarget(
+    target: String,
+    ignoreCase: Boolean = false,
+    targetBlock: AnnotatedString.Builder.(String) -> Unit,
+): AnnotatedString {
+    val content = this@fAnnotatedWithTarget
+    val list = splitFully(
+        target = target,
+        ignoreCase = ignoreCase,
+    )
+    return buildAnnotatedString {
+        val builder = this@buildAnnotatedString
+        if (list.isEmpty()) {
+            append(content)
+        } else {
+            list.forEach { item ->
+                if (item == target) {
+                    builder.targetBlock(target)
+                } else {
+                    append(item)
+                }
+            }
+        }
+    }
+}
+
+private fun String.splitFully(
+    target: String,
+    ignoreCase: Boolean = false,
+): List<String> {
+    val list = split(target, ignoreCase = ignoreCase)
+    if (list.isEmpty()) return emptyList()
+    if (list.size == 1) {
+        val item = list.first()
+        if (item == this) return emptyList()
+    }
+    return mutableListOf<String>().apply {
+        list.forEachIndexed { index, item ->
+            if (item.isEmpty()) {
+                add(target)
+            } else {
+                add(item)
+                if (index != list.lastIndex) {
+                    add(target)
+                }
+            }
+        }
+    }
+}
