849230117b03416928aea93eb2dc98b3359f8890
zjun <565061763@qq.com>
2024-09-14 00:05:23 +0800

    refactor click

--- lib/src/main/java/com/sd/lib/compose/utils/Click.kt
+++ lib/src/main/java/com/sd/lib/compose/utils/Click.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.Indication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.semantics.Role
 
 /**
@@ -20,16 +18,14 @@ fun Modifier.fClick(
    onClickLabel: String? = null,
    role: Role? = null,
    onClick: () -> Unit,
-) = this.composed {
-   clickable(
-      interactionSource = interactionSource ?: remember { MutableInteractionSource() },
-      indication = indication,
-      enabled = enabled,
-      onClickLabel = onClickLabel,
-      role = role,
-      onClick = onClick,
-   )
-}
+) = clickable(
+   interactionSource = interactionSource,
+   indication = indication,
+   enabled = enabled,
+   onClickLabel = onClickLabel,
+   role = role,
+   onClick = onClick,
+)
 
 /**
  * 点击事件，不带涟漪效果
@@ -45,16 +41,14 @@ fun Modifier.fCombinedClick(
    onLongClick: (() -> Unit)? = null,
    onDoubleClick: (() -> Unit)? = null,
    onClick: () -> Unit,
-) = this.composed {
-   combinedClickable(
-      interactionSource = interactionSource ?: remember { MutableInteractionSource() },
-      indication = indication,
-      enabled = enabled,
-      onClickLabel = onClickLabel,
-      role = role,
-      onLongClickLabel = onLongClickLabel,
-      onLongClick = onLongClick,
-      onDoubleClick = onDoubleClick,
-      onClick = onClick,
-   )
-}
+) = combinedClickable(
+   interactionSource = interactionSource,
+   indication = indication,
+   enabled = enabled,
+   onClickLabel = onClickLabel,
+   role = role,
+   onLongClickLabel = onLongClickLabel,
+   onLongClick = onLongClick,
+   onDoubleClick = onDoubleClick,
+   onClick = onClick,
+)
