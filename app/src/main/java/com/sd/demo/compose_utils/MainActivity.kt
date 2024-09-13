f72a692d474d2a605b2bbcbd7fa6c9c4be6eadb1
zjun <565061763@qq.com>
2024-09-14 00:10:38 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_utils/MainActivity.kt
+++ app/src/main/java/com/sd/demo/compose_utils/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
          AppTheme {
             Content(
                listActivity = listOf(
-
+                  SampleClick::class.java,
                ),
                onClickActivity = {
                   startActivity(Intent(this, it))
