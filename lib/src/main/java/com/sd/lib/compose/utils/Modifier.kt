3e9df910ab82279a6096b89a2558dd19ceaa28da
zjun <565061763@qq.com>
2024-09-14 21:18:19 +0800

    fConsumePointerEvent

---
+++ lib/src/main/java/com/sd/lib/compose/utils/Modifier.kt
@@ -0,0 +1,18 @@
+package com.sd.lib.compose.utils
+
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
+
+/**
+ * 消费所有指针事件
+ */
+fun Modifier.fConsumePointerEvent(
+   pass: PointerEventPass = PointerEventPass.Initial,
+): Modifier = pointerInput(Unit) {
+   awaitEachGesture {
+      val event = awaitPointerEvent(pass)
+      event.changes.forEach { it.consume() }
+   }
+}
