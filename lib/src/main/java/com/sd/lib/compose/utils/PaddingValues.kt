0ea8fe42dce7f7cff8f171ce681d9805193bdee0
zjun <565061763@qq.com>
2024-10-18 14:39:15 +0800

    PaddingValues

---
+++ lib/src/main/java/com/sd/lib/compose/utils/PaddingValues.kt
@@ -0,0 +1,37 @@
+package com.sd.lib.compose.utils
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.Dp
+
+@Composable
+operator fun PaddingValues.plus(paddingValues: PaddingValues): PaddingValues {
+   return PaddingValues(
+      top = this.top + paddingValues.top,
+      bottom = this.bottom + paddingValues.bottom,
+      start = this.start + paddingValues.start,
+      end = this.end + paddingValues.end,
+   )
+}
+
+val PaddingValues.top: Dp
+   @Composable @ReadOnlyComposable get() = calculateTopPadding()
+
+val PaddingValues.bottom: Dp
+   @Composable @ReadOnlyComposable get() = calculateBottomPadding()
+
+val PaddingValues.start: Dp
+   @Composable get() {
+      val layoutDirection = LocalLayoutDirection.current
+      return calculateStartPadding(layoutDirection)
+   }
+
+val PaddingValues.end: Dp
+   @Composable get() {
+      val layoutDirection = LocalLayoutDirection.current
+      return calculateEndPadding(layoutDirection)
+   }
