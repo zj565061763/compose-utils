bac67418cc23704f479d45dd9e332f77d14714d2
zjun <565061763@qq.com>
2024-11-07 00:40:23 +0800

    fAtLeastState

--- lib/src/main/java/com/sd/lib/compose/utils/Utils.kt
+++ lib/src/main/java/com/sd/lib/compose/utils/Utils.kt
@@ -1,28 +1,14 @@
 package com.sd.lib.compose.utils
 
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
-import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.flow.first
 import kotlin.coroutines.cancellation.CancellationException
-import kotlin.coroutines.resume
 
 internal suspend fun Lifecycle.fAtLeastState(
    state: Lifecycle.State = Lifecycle.State.STARTED,
 ): Boolean {
    if (currentState == Lifecycle.State.DESTROYED) throw CancellationException()
    if (currentState.isAtLeast(state)) return true
-   suspendCancellableCoroutine { continuation ->
-      val observer = object : LifecycleEventObserver {
-         override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-            if (event.targetState.isAtLeast(state)) {
-               removeObserver(this)
-               continuation.resume(Unit)
-            }
-         }
-      }
-      addObserver(observer)
-      continuation.invokeOnCancellation { removeObserver(observer) }
-   }
+   currentStateFlow.first { it.isAtLeast(state) }
    return false
 }
