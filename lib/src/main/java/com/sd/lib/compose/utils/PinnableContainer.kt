78cb49c7ac06d16a4f5c3a3d1505c304e79e6c59
zjun <565061763@qq.com>
2024-12-04 15:02:05 +0800

    pin

---
+++ lib/src/main/java/com/sd/lib/compose/utils/PinnableContainer.kt
@@ -0,0 +1,19 @@
+package com.sd.lib.compose.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.layout.LocalPinnableContainer
+import androidx.compose.ui.layout.PinnableContainer.PinnedHandle
+
+@Composable
+fun pin(): PinnedHandle? {
+   val container = LocalPinnableContainer.current
+   val pinnedHandle = remember(container) { container?.pin() }
+   if (pinnedHandle != null) {
+      DisposableEffect(pinnedHandle) {
+         onDispose { pinnedHandle.release() }
+      }
+   }
+   return pinnedHandle
+}
